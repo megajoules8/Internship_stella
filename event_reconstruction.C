6fa0a3f90fb4e798a58a34702d57f78e225519c6
megajoules8 <35748747+megajoules8@users.noreply.github.com>
2021-05-08 12:32:30 +0200

    Update event_reconstruction.C

--- event_reconstruction.C
+++ event_reconstruction.C
@@ -1,15 +1,21 @@
 //event reconstruction
- #include "libPerso.h"
-//implement event recontruction in a later iteration of this script
-//imlement automatic fitting in this script on only the qdc side
-Double_t my_gaus (Double_t *var, Double_t *par){
+//fits correlation plot and provides projection plot parameters 
+#include "libPerso.h"
+
+Double_t my_gaus (Double_t *var, Double_t *par){ //like y(x,t)
   // par[0]: amplitude
   // par[1]: mean
   // par[2]: sigma
   
-  return par[0]*TMath::Exp (-.5*TMath::Power ((var[0] - par[1])/par[2], 2));
+  return par[0]*TMath::Exp (-.5*TMath::Power ((var[0] - par[1])/par[2], 2)); // x,n
 }
 
+Double_t myLin (Double_t *var, Double_t *par){
+  // par[0]: offset
+  // par[1]: slope
+
+  return par[0] + par[1]*var[0];
+}
 
 void event_reconstruction (){
 
@@ -26,33 +32,46 @@ void event_reconstruction (){
   const Double_t roi02_high = 16.e3;
   const Double_t roi04_low = 7.e3;
   const Double_t roi04_high = 13.e3;
-  const Int_t thresh[1] = {100}; //saying fitting 1 histogram, so only need one threshold 
+  const Int_t thresh[1] = {20}; //saying fitting 1 histogram, so only need one threshold, changed from 100 on 20/04
   
   UShort_t Energy, Channel; // 16 bit int
   ULong64_t Timestamp;     // 64 bit int
   
   TChain *f_in = new TChain ("Data_R");
-  f_in->Add ("/home/megajoules/projects/check_compass/run01_2/RAW/SDataR_run01_2.root");
+  f_in->Add ("/home/megajoules/projects/check_compass/run_1/RAW/SDataR_run_1.root");
+  //f_in->Add ("/home/megajoules/projects/check_compass/run_2/RAW/SDataR_run_2.root");
   //f_in->Add ("/home/mheine/Work/Tmp/run01_2/RAW/SDataR_run01_1.root");
-  f_in->SetBranchAddress ("Energy"   , &Energy );
-  f_in->SetBranchAddress ("Channel"  , &Channel );  // 0, 2, 4
+  f_in->SetBranchAddress ("Energy"   , &Energy ); //Energy in the raw data is a misnomer! it refers to the channel, i.e. the uncalibrated value
+  f_in->SetBranchAddress ("Channel"  , &Channel );  // 0, 2, 4 corresponding to the junction 1, 2, and the QDC channel
   f_in->SetBranchAddress ("Timestamp", &Timestamp); // ps
 
 
   // data container
   const Double_t qdc_low = 0e3;
-  const Double_t qdc_high = 20e3;
+  const Double_t qdc_high = 25e3; //changed from 20e3 on 20/04
   const Int_t qdc_bin = (qdc_high - qdc_low)/10;
   const Double_t bin2Qdc = (qdc_high - qdc_low)/qdc_bin;
+
+  //for calibrated energy
+  const Double_t Energy_low = 0e3; //correct ranges here
+  const Double_t Energy_high = 8000;
+  const Int_t Energy_bin = (Energy_high - Energy_low)/10; //choose appropriate binning
+  const Double_t bin2Energy = (Energy_high - Energy_low)/Energy_bin;
+  std:: cout << bin2Qdc << endl;
+  //const Float_t Shift = 181.063; //shift and slope privided by energy_calibration.C
+  //const Float_t Slope = 0.413141;
+  const Float_t Shift = 183.115; //shift and slope privided by energy_calibration.C run_01
+  const Float_t Slope = 0.411245;
  
 
-  TH1F *h_qdc[6], *h_qdc_jj[4];
+  TH1F *h_qdc[6], *h_qdc_jj[4], *h_Energy_calib[3]; //declaring TH1f variables [which is an array of objects of the class with a specified length]
+  TH2F *h_qdc_qj;
+  //h_qdc is an array with a size 6, each of those 6 slots is a pointer to an object, those objects are in the class TH1F
  
  
-  for (Int_t i = 0; i < 3; i++)
-    {
+  for (Int_t i = 0; i < 3; i++){
       h_qdc[i] = new TH1F (Form ("h_qdc[%i]", i), "", qdc_bin, qdc_low, qdc_high);
-      h_qdc[i + 3] = new TH1F (Form ("h_qdc[%i]", i + 3), "", qdc_bin, qdc_low, qdc_high);
+      h_qdc[i + 3] = new TH1F (Form ("h_qdc[%i]", i + 3), "", qdc_bin, qdc_low, qdc_high); //initialises 3, 4, 5
       h_qdc[i + 3]->SetLineColor (2);
 
       h_qdc_jj[i] = new TH1F (Form ("h_qdc_jj[%i]", i), "", qdc_bin, qdc_low, qdc_high);
@@ -60,7 +79,13 @@ void event_reconstruction (){
     }
   h_qdc_jj[3] = new TH1F ("h_qdc_jj[3]", "", 2*qdc_bin, qdc_low, 2*qdc_high);
   h_qdc_jj[3]->SetLineColor (2);
-  
+  h_qdc_qj = new TH2F ("h_qdc_qj", "", 2*qdc_bin, qdc_low, 2*qdc_high, qdc_bin, qdc_low, qdc_high);
+
+  //calibrated energy
+  for (Int_t i = 0; i < 3; i++){
+      h_Energy_calib[i] = new TH1F (Form ("h_Energy_calib[%i]", i), "", Energy_bin, Energy_low, Energy_high);
+  }
+
   // data loop
   for (Long64_t i = 0; i < (Long64_t) f_in->GetEntries () - 10; i++)
     {
@@ -68,6 +93,8 @@ void event_reconstruction (){
       if (i%((Long64_t) f_in->GetEntries ()/10) == 0) std::cout << "  " << std::setw (3) << (Int_t) (100.*i/f_in->GetEntries () + 0.5) << "% done" << std::endl;
 
       h_qdc[(Int_t) Channel/2]->Fill (Energy); // QDC spectra straight
+
+      h_Energy_calib[(Int_t) Channel/2]->Fill (Shift + Slope*Energy); //Energy spectra
 
         // make some events
       std::vector <Long64_t> time_coi;
@@ -97,11 +124,14 @@ void event_reconstruction (){
       {
 
           // last element isn't coincident
-	      time_coi.pop_back ();
+	        time_coi.pop_back ();
       	  qdc_coi.pop_back ();
       	  chn_coi.pop_back ();
 	  
-      	  for (Int_t j = 1; j < (Int_t) time_coi.size (); j++) h_qdc[(Int_t) chn_coi.at (j)/2]->Fill (qdc_coi.at (j)); // QDC spectra straight (which we'd jump over)
+      	  for (Int_t j = 1; j < (Int_t) time_coi.size (); j++) { //need same number of counts in coi and non-coi hists
+            h_qdc[(Int_t) chn_coi.at (j)/2]->Fill (qdc_coi.at (j)); // QDC spectra straight (which we'd jump over)
+            h_Energy_calib[(Int_t) chn_coi.at (j)/2]->Fill (Shift + Slope*(qdc_coi.at (j))); //Energy spectra 
+          }
           if  ((Int_t) time_coi.size () == 2) // two coincident
 	        {
 	        if (chn_coi.at (0) == 2 || chn_coi.at (1) == 2) // coincidence of Ji + Q1
@@ -138,25 +168,18 @@ void event_reconstruction (){
 	      while (iSwapped);
 
 	      h_qdc_jj[3]->Fill (qdc_coi.at (0) + qdc_coi.at (2)); // total charge junction side
-	      //h_qdc_qj->Fill (qdc_coi.at (0) + qdc_coi.at (2), qdc_coi.at (1));
+	      h_qdc_qj->Fill (qdc_coi.at (1), qdc_coi.at (0) + qdc_coi.at (2)); //x,y
         
 	      
-	      for (Int_t j = 0; j < 3; j++) h_qdc[(Int_t) chn_coi.at (j)/2 + 3]->Fill (qdc_coi.at (j)); // triple coincident spectra
-      	    
+	      for (Int_t j = 0; j < 3; j++) { // triple coincident spectra
+          h_qdc[(Int_t) chn_coi.at (j)/2 + 3]->Fill (qdc_coi.at (j));
+        } // triple coincident spectra
+        //one line loop, can put a single statement but better for readability to not
 	  	  
       	  i += (Int_t) time_coi.size () - 1; // increment at beginning of next turn, += refers to how much you add each increment 
             
       	}
-        
-
-
-
-
-        /*for (Int_t j = 0; j < 3; j++) h_qdc[(Int_t) chn_coi.at (j)/2 + 3]->Fill (qdc_coi.at (j)); // triple coincident spectra
-      	  {
-	  	  
-      	  */
-        
+               
       }   
     }
     
@@ -185,7 +208,6 @@ void event_reconstruction (){
     {
       c_qdc->cd (i + 1)->SetTickx ();
       c_qdc->cd (i + 1)->SetTicky ();
-      //      c_h_qdc_qjqdc->cd (i + 1)->SetLogy ();
       h_qdc[i]->GetXaxis ()->SetRangeUser (100., 16.e3);
       h_qdc[i]->Draw ();
       h_qdc[i + 3]->Draw ("SAME");
@@ -209,15 +231,29 @@ void event_reconstruction (){
     }
   c_qdc_j->Print (Form ("run%03i_check_compass_c_qdc_j.pdf", n_run));
 
+  //Calibrated Energy Spectra
+   TCanvas *c_Energy_calib = new TCanvas ("c_Energy", "c_Energy", 700, 500);
+  c_Energy_calib->Draw ();
+
+  
+      c_Energy_calib->cd ()->SetTickx ();
+      c_Energy_calib->cd ()->SetTicky ();
+      h_Energy_calib[1]->GetXaxis ()->SetRangeUser (100., 8000.); //ohmic side =2 then divided by 2
+      h_Energy_calib[1]->Draw ();
+  
+
+
+
+
   //fitting
   // scan histograms for crude fit parameters
   Double_t ampl[4][3], mean[4][3], sigma[4][3];
-  for (Int_t i = 1; i < 2; i++) // 4
+  for (Int_t i = 0; i < 4; i++) // 4 //change 20/04
     {
       Int_t peak = 0;
       
       //      for (Int_t j = 1; j < (Int_t) h_qdc[i]->GetNbinsX (); j++) // avoid underflow , r001
-      for (Int_t j = 500; j < (Int_t) h_qdc[i]->GetNbinsX (); j++) // avoid base line , r003
+      for (Int_t j = 500; j < (Int_t) h_qdc[i]->GetNbinsX (); j++) // avoid base line , r003 //changed j from 500 on 20/04 caused crashing
 	    {
 	        if (h_qdc[i]->GetBinContent (j) > thresh[i])
 	        {	      
@@ -229,18 +265,19 @@ void event_reconstruction (){
 	         mean[i][peak] = qdc_low + h_qdc[i]->GetMaximumBin ()*bin2Qdc;
 	         sigma[i][peak] = k/2*bin2Qdc;
 	      
-             std::cout << i << " " << peak << " " << ampl[i][peak] << " " << mean[i][peak] << " " << sigma[i][peak] << std::endl;
-	      
+          //std::cout << i << " " << peak << " " << ampl[i][peak] << " " << mean[i][peak] << " " << sigma[i][peak] << ""<< j << ""<< j+k << std::endl;
+	        
 	        peak++;
 	        j += k + 4; // avoid fluctuations
 	        }
 	    }
       h_qdc[i]->GetXaxis ()->SetRange (0, qdc_bin);
+    
       
     }
    //fitting display
-   TF1 *f_fit = new TF1 ("f_fit", my_gaus, 10.e3, 20.e3, 3);
-     f_fit->SetNpx (1e3);
+   TF1 *f_fit = new TF1 ("f_fit", my_gaus, 10.e3, 25.e3, 3); //instantiating new TF1 object with the parameters it needs to do that, returns the pointer f_fit //*changed 20/04
+     f_fit->SetNpx (1e3); 
      f_fit->SetParLimits (0, 0., 10.e6);
      f_fit->SetParLimits (2, 0., 10.e6);
 
@@ -256,14 +293,13 @@ void event_reconstruction (){
       h_qdc[i]->Draw ();
 
       // fits
-      for (Int_t j = 0; j < 3; j++) //select peak numbers 0, 1, and 2
-	    {
+      for (Int_t j = 0; j < 3; j++){ //select peak numbers 0, 1, and 2
 	        // adjustment
 	        f_fit->SetRange (mean[i][j] - 1.*sigma[i][j], mean[i][j] + 1.*sigma[i][j]);
 	        f_fit->SetParameters (ampl[i][j], mean[i][j], sigma[i][j]);
 	        //	  std::cout << " " << j << " " << f_fit->GetParameter (0) << " " << f_fit->GetParameter (1) << " " << f_fit->GetParameter (2) << std::endl;
 
-	        h_qdc[i]->Fit (f_fit, "RQI");
+	        h_qdc[i]->Fit (f_fit, "RI");
 	        ampl[i][j] = f_fit->GetParameter (0);
 	        mean[i][j] = f_fit->GetParameter (1);
 	        sigma[i][j] = f_fit->GetParameter (2);
@@ -275,7 +311,35 @@ void event_reconstruction (){
 	        mean[i][j] = f_fit->GetParameter (1);
 	        sigma[i][j] = f_fit->GetParameter (2);
             f_fit->DrawCopy ("SAME");
+            std::cout << " " <<ampl[i][j] << " " << mean[i][j] << " " << sigma[i][j] << std::endl;
         }
     } 
     c_qdc->Print (Form ("run%03i_check_compass_c_qdc.pdf", n_run));
+
+    TCanvas *c_corr = new TCanvas ("c_corr", "c_corr", 700, 500);
+    c_corr->Draw ();
+    c_corr->cd ()->SetTickx ();
+    c_corr->cd ()->SetTicky ();
+    //      c_corr->cd ()->SetLogy ();
+
+    h_qdc_qj->GetYaxis ()->SetTitle ("QDC junction"); //fix this
+    h_qdc_qj->GetXaxis ()->SetTitle ("QDC charge");
+    //h_qdc_qj->Draw ("COLZ");
+  
+    c_corr->Print (Form ("run%03i_check_compass_c_corr.pdf", n_run));
+
+     TF1 *corr_fit = new TF1 ("corr_fit", myLin, 5000., 25000., 2);
+    // display and fit
+    corr_fit->SetNpx (1e3); 
+    corr_fit->SetParameters (1000, 5/3 );
+
+    h_qdc_qj->Fit (corr_fit, "RI");
+  
+    h_qdc_qj->DrawCopy ("SAME");
+  
 }
+
+//uncalibrated ohmic side fitting:
+//peak1:
+//peak2:
+//peak3: 89.92=/- 3.52, 13650+/-3.347, 57.74+/-3.20
